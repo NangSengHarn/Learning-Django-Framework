bd5a3c5415b2ae2594532797fb0be360c8b6c9db
NangSengHarn <nangsengharn@gmail.com>
2022-02-28 13:41:36 +0630

    queryset and passing datas to templates

--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from accounts.models import *
 
 def customer(request):
     return render(request,'accounts/customer.html')
 
 def product(request):
-    return render(request,'accounts/product.html')
+    products=Product.objects.all()
+    return render(request,'accounts/product.html',{
+        'products':products
+    })
 
 def dashboard(request):
-    return render(request,'accounts/dashboard.html')
+    customers=Customer.objects.all()
+    orders=Order.objects.all()
+    total=orders.count()
+    delivered=Order.objects.filter(status="delivered").count()
+    pending=Order.objects.filter(status="pending").count()
+
+    return render(request,'accounts/dashboard.html',{
+        'customers':customers,
+        'orders':orders,
+        'total':total,
+        'delivered':delivered,
+        'pending':pending
+    })
